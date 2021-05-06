30d2de811e076408cf029135adf7e7ae69c6b35b
Myron819 <19036849@rp.edu.sg>
2021-05-06 20:41:26 +0000

    Initial commit

---
+++ app/src/main/java/com/example/demodatabase/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.demodatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnInsert;
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask("Submit RJ", "25 Apr 2016");
+                db.close();
+            }
+        });
+
+
+    }
+}
